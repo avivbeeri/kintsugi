f67e871258d6a6b56331dcaf1eea63a5c5781e3d
Aviv Beeri <avbeeri@gmail.com>
2024-04-02 07:46:12 +0100

    [fix #7] Bare minimum JEI support

---
+++ src/main/java/net/infinitelimit/kintsugi/compat/JEIKintsugiModPlugin.java
@@ -0,0 +1,28 @@
+package net.infinitelimit.kintsugi.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import net.infinitelimit.kintsugi.Kintsugi;
+import net.infinitelimit.kintsugi.KnowledgeHelper;
+import net.infinitelimit.kintsugi.item.KnowledgeBookItem;
+import net.infinitelimit.kintsugi.item.ModItems;
+import net.minecraft.resources.ResourceLocation;
+
+@JeiPlugin
+public class JEIKintsugiModPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(Kintsugi.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerItemSubtypes(ISubtypeRegistration registration) {
+        IModPlugin.super.registerItemSubtypes(registration);
+        registration.registerSubtypeInterpreter(
+                ModItems.KNOWLEDGE_BOOK.get(),
+                (itemStack, uidContext) -> KnowledgeHelper.getEnchantment(itemStack).getDescriptionId()
+        );
+    }
+}
